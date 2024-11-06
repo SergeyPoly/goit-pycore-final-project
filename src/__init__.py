79f974bb3e9270fd1dce2375e931e7b5f3a37237
v.dmytriiev <v.dmytriiev@sdventures.com>
2024-11-06 11:52:18 +0100

    Додав логіку autocomplete, requirements.txt, корисні скрипти

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,13 @@
 from src.helpers import parse_input
 from src.bot_commands import commands, exit_commands
 from src.serializers import load_data, save_data
+from src.autocompletion import get_autocomplete_input
 
-__all__ = ["parse_input", "commands", "exit_commands", "load_data", "save_data"]
+__all__ = [
+    "parse_input",
+    "commands",
+    "exit_commands",
+    "load_data",
+    "save_data",
+    "get_autocomplete_input",
+]
